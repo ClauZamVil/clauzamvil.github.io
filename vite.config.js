8f87a87c842cd1a295ff0b91b8941abd2d0466d2
ClauZamVil <claudia.vilches.zamora@gmail.com>
2026-06-17 21:16:25 -0400

    Update Vite config base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+// En tu archivo vite.config.js
 export default defineConfig({
   plugins: [react()],
-  // Esto es lo que permite que el navegador encuentre los archivos en la subcarpeta
-  base: '/Clauzamvil/' 
+  base: '/' // <-- IMPORTANTE: Pon '/' porque es el dominio principal
 })
